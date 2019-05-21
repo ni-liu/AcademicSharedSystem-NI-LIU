b2d5f68b82415007d8c70b55b5c6182b4a583e0c
Ian <LiuQiZheng98@163.com>
2019-05-21 18:58:00 +0800

    Create BlogAuthorityEnum.java
    Co-Authored-By: liuqizheng98 <liuqizheng98@users.noreply.github.com>

---
+++ src/main/java/com/javaee/ass/entity/enums/BlogAuthorityEnum.java
@@ -0,0 +1,25 @@
+package com.javaee.ass.entity.enums;
+
+/**
+ * 博客权限
+ * 1、私人
+ * 2、公共
+ */
+public enum BlogAuthorityEnum {
+    AUTHORITY_PRIVATE(1) ,
+    AUTHORITY_PUBLIC(2);
+
+    private int authority;
+
+    BlogAuthorityEnum(int authority) {
+        this.authority = authority;
+    }
+
+    public int getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(int authority) {
+        this.authority = authority;
+    }
+}
