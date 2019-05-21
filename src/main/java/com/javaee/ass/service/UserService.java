2ab343853759430f7477bebdf47f6398847bb8a8
Ian <LiuQiZheng98@163.com>
2019-05-21 18:58:30 +0800

    Create UserService.java
    Co-Authored-By: liuqizheng98 <liuqizheng98@users.noreply.github.com>

---
+++ src/main/java/com/javaee/ass/service/UserService.java
@@ -0,0 +1,24 @@
+package com.javaee.ass.service;
+
+import com.javaee.ass.dao.UserDAO;
+import com.javaee.ass.entity.role.UserDO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService {
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public UserDO searchByPkId(String pkId) {
+        return userDAO.searchByPkId(pkId);
+    }
+
+    public String searchNicknameByPkIs(String pkId) {
+        return userDAO.searchNicknameByPkId(pkId);
+    }
+}
