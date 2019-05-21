f623f2c2d340aa1ee03ed1e0d98291b30cc1f6ff
Nisson-CD <diorcd@outlook.com>
2019-05-21 19:06:03 +0800

    Create FinalVariablesUtils.java
    Co-Authored-By: nisson-cd <nisson-cd@users.noreply.github.com>

---
+++ src/main/java/com/javaee/ass/utils/FinalVariablesUtils.java
@@ -0,0 +1,18 @@
+package com.javaee.ass.utils;
+
+public class FinalVariablesUtils {
+    public static final int COURSE_PAGE_SIZE = 8;
+    public static final String ORACLE_JDBC_DRIVER = "oracle.jdbc.OracleDriver";
+    public static final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
+    public static final String DB_USER = "C##ARU";
+    public static final String DB_PASSWORD = "aru20161903";
+    public static final String DEFAULT_COURSE_PIC_OSS_URL =
+            "https://academic-shared-system.oss-cn-shanghai.aliyuncs.com/courses/pic/DefaultCoursePic.jpg";
+    public static final String DEFAULT_COURSE_OUTLINE_OSS_URL =
+            "https://academic-shared-system.oss-cn-shanghai.aliyuncs.com/courses/outlines/CourseOutline%28Default%29.docx";
+
+    public static final String OSS_ENDPOINT = "oss-cn-shanghai.aliyuncs.com";
+    public static final String OSS_ACCESS_KEY_ID = "LTAIcl4rmogiELkq";
+    public static final String OSS_ACCESS_KEY_SECRET = "oVcDZFSoqJb0me5fkMRnvxTpwqjKGE";
+    public static final String OSS_BUCKET = "academic-shared-system";
+}
